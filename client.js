39b75415d145f281510e4bc270b937843b533d61
GaganTut <gstut@live.com>
2017-04-12 17:57:53 -1000

    Users can now have own usernames

--- client.js
+++ client.js
@@ -2,7 +2,6 @@
 const net = require('net');
 
 const client = net.connect({port: 3000}, () => {
-  console.log('Connected to Server!!!');
 
   client.on('data', (data) => {
     console.log(data.toString());
--- server.js
+++ server.js
@@ -6,15 +6,30 @@ const chatHistory = [];
 
 const server = net.createServer((connection) => {
   clientArr.push(connection);
+  connection.userName = 'Anonymous';
+  connection.write('For Username, type $userName');
 
   connection.on('data', (data) => {
-    chatHistory.push(data.toString());
-    console.log(`${data.toString()}`);
-    for (let i = 0; i < clientArr.length; i++) {
-      if (connection !== clientArr[i]) {
-        clientArr[i].write(data);
-      }
+    if (data.toString().charAt(0) === '$') {
+      clientArr.forEach((element) => {
+        if (element === connection) {
+          connection.userName = data.toString()
+            .split("")
+            .filter((element) => {return element !== '$';})
+            .join("");
+        }
+      });
+    } else {
+      chatHistory.push(data.toString());
+      clientArr.forEach((element) => {
+        if (element !== connection) {
+          element.write(`${connection.userName}: ${data.toString()}\n-----------------`);
+        } else {
+          element.write(`-----------------`);
+        }
+      });
     }
+    console.log(`${connection.userName}: ${data.toString()}`);
   });
 
   connection.on('end', () => {
