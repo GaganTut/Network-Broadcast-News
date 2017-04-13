b3bbd31fae68119e24116b8d28475ff27ca19189
GaganTut <gstut@live.com>
2017-04-12 18:49:03 -1000

    Clinets can now send private messages

--- server.js
+++ server.js
@@ -3,31 +3,48 @@ const net = require('net');
 
 const clientArr = [];
 const chatHistory = [];
+const specialKeys = `
+  '?' : 'Help',\n
+  '$' : 'Set Username',\n
+  '/' : 'Secret Message to Server Only',\n
+  '@' : 'Attach Username to Send Private'
+`;
 
 const server = net.createServer((connection) => {
   clientArr.push(connection);
   connection.userName = 'Anonymous';
-  connection.write('For Username, type $userName');
+  connection.write(`${specialKeys}\nENJOY CHATTING!`);
 
   connection.on('data', (data) => {
-    if (data.toString().charAt(0) === '$') {
-      clientArr.forEach((element) => {
-        if (element === connection) {
-          connection.userName = data.toString()
-            .split("")
-            .filter((element) => {return element !== '$';})
-            .join("");
-        }
-      });
-    } else {
-      chatHistory.push(data.toString());
-      clientArr.forEach((element) => {
-        if (element !== connection) {
-          element.write(`${connection.userName}: ${data.toString()}\n-----------------`);
-        } else {
-          element.write(`-----------------`);
-        }
-      });
+    switch (data.toString().charAt(0)) {
+      case '@' :
+        let privUser = data.toString().slice(1, data.toString().indexOf(' '));
+        let message = data.toString().slice(data.toString().indexOf(' ')).slice(1);
+        clientArr.forEach((element) => {
+          if (element.userName === privUser) {
+            element.write(`***${connection.userName} - PRIVATE - : ${data.toString()}`);
+          }
+        });
+        break;
+      case '?' :
+        connection.write(`${specialKeys}`);
+        break;
+      case '$' :
+        clientArr.forEach((element) => {
+          if (element === connection) {
+            connection.userName = data.toString().slice(1, -1);
+          }
+        });
+        break;
+      default:
+        chatHistory.push(data.toString());
+        clientArr.forEach((element) => {
+          if (element !== connection) {
+            element.write(`${connection.userName}: ${data.toString()}\n-----------------`);
+          } else {
+            element.write(`-----------------`);
+          }
+        });
     }
     console.log(`${connection.userName}: ${data.toString()}`);
   });
